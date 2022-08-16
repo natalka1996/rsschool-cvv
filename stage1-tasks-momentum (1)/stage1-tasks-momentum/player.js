58b810c2b3010b2ef5096b38ec913a2744115b04
Natalka1996 <nata-chekavaya@mail.ru>
2022-08-17 00:34:46 +0300

    feat: new task momentum

---
+++ stage1-tasks-momentum (1)/stage1-tasks-momentum/player.js
@@ -0,0 +1,23 @@
+const playList = [
+    {
+      title: 'River Flows In You',
+      src: '../assets/sounds/River Flows In You.ogg',
+      duration: '01:37'
+    },
+    {
+      title: 'Ennio Morricone',
+      src: '../assets/sounds/Ennio Morricone.ogg',
+      duration: '01:37'
+    },
+    {
+      title: 'Summer Wind',
+      src: '../assets/sounds/Summer Wind.og',
+      duration: '03:50'
+    },
+    {
+      title: 'Ambient',
+      src: '../assets/sounds/Music Ambient.ogg',
+      duration: '00:14'
+    }
+  ]
+  export default playList;
