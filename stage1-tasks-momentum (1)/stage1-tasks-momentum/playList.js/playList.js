fa36dda646921023106dfe7aff36b98545b4c8ce
Natalka1996 <nata-chekavaya@mail.ru>
2022-08-16 23:53:21 +0300

    fix: file playlist

--- stage1-tasks-momentum (1)/stage1-tasks-momentum/playList.js/playList.js
+++
@@ -1,9 +0,0 @@
-import playList from './playList.js';
-console.log(playList);
-const audio = new Audio();
-
-function playAudio() {
-  audio.src = '../assets/sounds/Aqua Caelestis.mp3'
-  audio.currentTime = 0;
-  audio.play();
-}
